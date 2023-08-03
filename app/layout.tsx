8357b5059b24800f69b32891638fad4b18a96073
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-08-03 18:58:35 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import Navbar from "@/components/Navbar"
 import "./globals.css"
 import Footer from "@/components/Footer"
 export const metadata = {
-  title: 'Flexibble App',
+  title: 'DavidLeo Tech Portfolio App',
   description: 'Showcase and discover remarkable developer project',
 }
+
+export const revalidate = 0;
 
 export default function RootLayout({
   children,
